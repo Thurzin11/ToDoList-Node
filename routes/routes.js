1dd3d511edd57c8ef391324c5373ee89abbf40c0
Arthur <arthursantosgoncalves587@gmail.com>
2024-10-31 15:07:56 -0300

    estilização

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@ const routes = require("express").Router();
 const taskController = require("../controller/TaskController");
 
 routes.get("/", taskController.getAllTasks);
-
 routes.post("/create", taskController.createTask);
-
+routes.get("/getById/:id/:method", taskController.getById);
+routes.post("/update/:id", taskController.update);
+routes.get("/delete/:id", taskController.deleteTask);
 module.exports = routes;
